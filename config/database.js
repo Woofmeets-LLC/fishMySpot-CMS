8cac24758e3f9f9cc23f1d15cb33bfe98efbd70d
Biswajit Das <biswajit@algosolver.com>
2022-04-22 12:29:52 +0600

    uploaded to gcp

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+const productionConnection = (env) => {
+  return {
+    client: 'postgres',
+    connection: {
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USER'),
+        password: env('DATABASE_PASSWORD'),
+    },
+    pool: {
+      min: 0,
+      max: 15, 
+      idleTimeoutMillis: 30000,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 3000
+    }
+  }   
+}
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+  connection: productionConnection(env),
+})
