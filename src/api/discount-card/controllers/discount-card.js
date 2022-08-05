2674d09c7194c5fd8e1c50193e8ec570ca53934d
Biswajit Das <biswajit@algosolver.com>
2022-08-05 05:47:53 +0000

    Dev

---
+++ src/api/discount-card/controllers/discount-card.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ *  discount-card controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::discount-card.discount-card");
---
+++ src/api/discount-card/services/discount-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * discount-card service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::discount-card.discount-card');
