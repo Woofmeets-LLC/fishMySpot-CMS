f72374f0daec4fe15713d4d7d707ce577eabe58f
Biswajit Das <biswajit@algosolver.com>
2022-08-05 11:36:44 +0600

    fix: PROJ-107 update strapi and backend url

--- src/api/discount-card/content-types/discount-card/lifecycles.js
+++ src/api/discount-card/content-types/discount-card/lifecycles.js
@@ -1,22 +1,37 @@
 const { default: axios } = require("axios");
+var createError = require("http-errors");
 
 module.exports = {
   async beforeCreate(event) {
-    let result = await axios.post(
-      `${process.env.BACKEND_URL}/giftcards/discount`,
-      {
-        amount: event.params.data.amount,
-        valid: event.params.data.valid,
-      }
-    );
-    event.params.data.coupon = result?.data?.promo;
+    try {
+      let result = await axios.post(
+        `${process.env.BACKEND_URL}/giftcards/discount`,
+        {
+          amount: event.params.data.amount,
+          valid: event.params.data.valid,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.BACKEND_TOKEN}`,
+          },
+        }
+      );
+      event.params.data.coupon = result?.data?.promo;
+    } catch (err) {
+      throw createError(400, "Failed to generate coupon code");
+    }
   },
   async beforeUpdate(event) {
     const { coupon } = event.params.data;
-    console.log({ event, coupon });
-    const { data } = await axios.get(
-      `${process.env.BACKEND_URL}/giftcards/${coupon}`
-    );
+    try {
+      await axios.get(`${process.env.BACKEND_URL}/giftcards/${coupon}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.BACKEND_TOKEN}`,
+        },
+      });
+    } catch (err) {
+      throw createError(403, "You are not allowed to modify coupon code.");
+    }
   },
   async beforeDeleteMany(event) {
     const deletedIds = event.params.where["$and"].map(
@@ -34,11 +49,19 @@ module.exports = {
       });
     const deletedCoupons = entries.map((entry) => entry.coupon);
 
-    await axios
-      .post(`${process.env.BACKEND_URL}/giftcards/discount/invalidate`, {
-        coupons: deletedCoupons,
-      })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+    axios
+      .post(
+        `${process.env.BACKEND_URL}/giftcards/discount/invalidate`,
+        {
+          coupons: deletedCoupons,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.BACKEND_TOKEN}`,
+          },
+        }
+      )
+      .then((res) => console.log(res?.data))
+      .catch((err) => console.log(err?.message));
   },
 };
