8b89aeb5635f30962749d34aafbfba105b7f592c
Biswajit Das <biswajit@algosolver.com>
2023-03-27 14:46:53 +0000

    Su 24

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const productionConnection = (env) => {
   return {
     client: "postgres",
     connection: {
-      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      host: env('DATABASE_HOST', `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
