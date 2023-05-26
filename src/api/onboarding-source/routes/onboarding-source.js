2c57a17ab1dc951ba878f9266cfd0928d7928dd9
Biswajit Das <biswajit@algosolver.com>
2023-05-26 09:48:09 +0000

    Dev

---
+++ src/api/onboarding-source/routes/onboarding-source.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * onboarding-source router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::onboarding-source.onboarding-source');
